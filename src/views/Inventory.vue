41218aa7faa7cee81d556fa6a48f5cbe514acdf6
jdburnham <jburnham@stu.southtexascollege.edu>
2020-04-27 17:42:51 -0500

    Added table to see all orders

--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -3,6 +3,7 @@
     <v-row justify="center">
       <v-btn color="amber lighten-2" @click.stop="adding = true">Add New Product</v-btn>
       <v-dialog v-model="adding" max-width="480">
+        <h1 class="mb-4">ALL PRODUCTS</h1>
         <v-card>
           <v-card-title class="headline">Add New Product</v-card-title>
           <v-card-text>
@@ -25,6 +26,39 @@
     <v-row justify="center" class="mt-8">
       <v-col cols="12">
         <v-data-table :headers="productsHeaders" :items="products" class="elevation-1"></v-data-table>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="mb-4">ALL ORDERS</h1>
+        <v-expansion-panels>
+          <v-expansion-panel v-for="order in orders" :key="order.id">
+            <v-expansion-panel-header>
+              <v-row>
+                <v-col cols="8"> Order # {{ order.id }} </v-col>
+                <v-col cols="$"> $ {{ order.order.total }} </v-col>
+              </v-row>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-list>
+                <v-list-item v-for="item in order.order.items" :key="item.id">
+                  <v-list-item-content>
+                    <v-list-item-title>{{ item.name }}</v-list-item-title>
+                  </v-list-item-content>
+                  <v-list-item-content>
+                    <v-list-item-subtitle>$ {{ item.price }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                  <v-list-item-content>
+                    <v-list-item-subtitle>{{ item.quantity }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                  <v-list-item-action>
+                    <v-list-item-subtitle>$ {{ (item.price * item.quantity).toFixed(2) }}</v-list-item-subtitle>
+                  </v-list-item-action>
+                </v-list-item>
+              </v-list>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
       </v-col>
     </v-row>
   </v-container>
@@ -53,6 +87,7 @@ export default {
         { text: 'Show', value: 'showCatalog' },
       ],
       products: [],
+      orders: [],
     }
   },
   created() {
@@ -75,6 +110,7 @@ export default {
     },
     async bind() {
       await this.$bind('products', db.collection('products'))
+      await this.$bind('orders', db.collection('orders'))
     },
   },
 }
